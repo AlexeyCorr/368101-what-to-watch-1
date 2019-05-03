1bd1a938875b41a661e2a9823aeee838dcc436e6
AlexeyCorr <alexeycorr@gmail.com>
2019-05-03 15:20:48 +0300

    Добавление propTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sprite from './../sprite/sprite.jsx';
 import MainScreen from './../main-screen/main-screen.jsx';
 
-const App = () => {
+const App = (props) => {
+  const {movieTitles} = props;
+
   return <div>
     <Sprite/>
-    <MainScreen/>
+    <MainScreen movieTitles={movieTitles}/>
   </div>;
+};
+
+App.propTypes = {
+  movieTitles: PropTypes.array.isRequired
 };
 
 export default App;
